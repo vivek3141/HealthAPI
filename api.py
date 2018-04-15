6cb64349e17eb9778ae4e04569c4f8998b5244f1
Vivek Verma <vivnps.verma@gmail.com>
2018-04-15 14:04:32 -0700

    Fixing API

--- api.py
+++ api.py
@@ -14,23 +14,24 @@ api = Api(app)
 todos = {}
 
 
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        barcode = request.args.get('img', type=str)
+class Barcode(Resource):
+    def get(self):
+        barcode = request.args.get('barcode', type=str)
         b = g.Barcode()
         ret = b.barcode(barcode)
         return ret
+    def put(self):
+        pass
+    #def put(self, todo_id):
+    #    todos[todo_id] = request.form['data']
+    #    return {todo_id: todos[todo_id]}
+    #def login(self, user, pas):
+    #    lo = l.Login()
+    #    ret = lo.login(user,pas)
+    #    return ret
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-    def login(self, user, pas):
-        lo = l.Login()
-        ret = lo.login(user,pas)
-        return ret
 
-
-api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(Barcode, '/')
 
 if __name__ == '__main__':
     app.run(debug=True,host=host)
